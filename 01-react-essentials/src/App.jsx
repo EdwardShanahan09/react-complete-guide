37e443439246963031cfbb611205eed2b6d6e47d
Edward Shanahan <edwardshanahan79@gmail.com>
2024-03-22 17:11:37 +0000

    Add CoreConcepts component

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -1,10 +1,29 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -15,8 +34,15 @@ function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Time to get started!</h2>
+      <main id="core-concepts">
+        <h2>Core Concepts</h2>
+
+        <ul>
+          <CoreConcepts {...CORE_CONCEPTS[0]} />
+          <CoreConcepts {...CORE_CONCEPTS[1]} />
+          <CoreConcepts {...CORE_CONCEPTS[2]} />
+          <CoreConcepts {...CORE_CONCEPTS[3]} />
+        </ul>
       </main>
     </div>
   );
