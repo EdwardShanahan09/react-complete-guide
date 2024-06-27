d5897e097e37a9928392d4a4b0d8073dba550032
Edward Shanahan <edwardshanahan79@gmail.com>
2024-06-27 16:47:06 +0100

    Add GameBoard component

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player/Player";
+import GameBoard from "./components/GameBoard/GameBoard";
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
         <Player initialName="Player 1" symbol="X" />
         <Player initialName="Player 2" symbol="O" />
       </ol>
+
+      <GameBoard />
     </div>
   );
 }
